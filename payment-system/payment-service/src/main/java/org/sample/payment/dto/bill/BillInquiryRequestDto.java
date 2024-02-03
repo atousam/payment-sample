2a27dc70ac2928adfe2bab78ee15f34cd869791b
Atousa <a.mirhosseini@hamrahcard.ir>
2024-02-03 22:30:21 +0330

    Added integration test

--- payment-system/payment-service/src/main/java/org/sample/payment/dto/bill/BillInquiryRequestDto.java
+++ payment-system/payment-service/src/main/java/org/sample/payment/dto/bill/BillInquiryRequestDto.java
@@ -1,12 +1,14 @@
 package org.sample.payment.dto.bill;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Atousa Mirhosseini
  * @since 29 Jan, 2024
  */
 @Getter
+@Setter
 public class BillInquiryRequestDto {
     private String billId;
 }
--- payment-system/payment-service/src/test/java/org/sample/payment/TestPaymentBillInquiry.java
+++ payment-system/payment-service/src/test/java/org/sample/payment/TestPaymentBillInquiry.java
@@ -5,5 +5,48 @@ package org.sample.payment;
  * Date:   2/3/2024
  */
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.sample.payment.dto.bill.BillInquiryRequestDto;
+import org.sample.payment.dto.bill.BillInquiryResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=none"})
 public class TestPaymentBillInquiry {
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(
+            "mysql:8.0-debian"
+    );
+
+    @Test
+    public void success() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        BillInquiryRequestDto requestDto = new BillInquiryRequestDto();
+        HttpEntity<BillInquiryRequestDto> request = new HttpEntity<>(requestDto, headers);
+        requestDto.setBillId("11223344");
+        ResponseEntity<BillInquiryResponseDto> response = restTemplate
+                .withBasicAuth("admin", "123456")
+                .exchange("http://localhost:" + port + "/bill/inquiry", HttpMethod.POST, request, BillInquiryResponseDto.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody().getPayId());
+    }
 }
